32256938e9fffcd15a793b0884f1e3294ecfbafb
Fo3nix <97182469+Fo3nix@users.noreply.github.com>
2023-07-01 21:06:26 +0200

    Solved another leetcode problem

--- src/main/java/SlidingWindow/Methods.java
+++ src/main/java/SlidingWindow/Methods.java
@@ -16,6 +16,26 @@ public class Methods {
         return currentWin;
     }
 
+    // lengthOfLongestSubstring
+    // https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/983974004/
+    public int lengthOfLongestSubstring(String s) {
+        int[] table = new int[95];
+
+        int maxL = 0;
+        int startI = 0;
+
+        for(int i =0; i<s.length(); i++){
+            startI = Math.max(startI, table[s.charAt(i)-32]);
+            maxL = Math.max(maxL, i-startI+1);
+            table[s.charAt(i)-32] = i+1;
+        }
+        maxL = Math.max(maxL, s.length()-startI);
+
+        return maxL;
+
+
+    }
+
 
 
 }
